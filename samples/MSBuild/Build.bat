1d37f06582785c211c078222f02da4e32be7226a
support@novuslogic.com <Think098>
2016-10-19 00:08:06 +1100

    Update 20161019

---
+++ samples/MSBuild/Build.bat
@@ -0,0 +1,3 @@
+call D:\Projects\Zautomatic\build\zautomatic.exe -project D:\Projects\Zautomatic\samples\MSBuild\TestApp.zap -projectconfig D:\Projects\Zautomatic\samples\MSBuild\TestApp.zapconfig 
+
+call pause
