66d67454941b1484ab7d20958010bc7598ee154a
ajohnston <ajohnston@AIMS54-1>
2018-05-25 16:28:27 +1000

    Option for Zcodegen and new function  degenex in CodeIMatic unit Include
    * New function codegenex in CodeIMatic unit Include
    
    * aPreCompatible option allowing for Zcodegen

--- source/Includes/CodeImatic.pas
+++ source/Includes/CodeImatic.pas
@@ -3,10 +3,10 @@ unit CodeImatic;
 interface
 
 Uses cmd;
-
+  
 function codegen(aProject: string; aProjectconfig: string; aVariables: string; aWorkingdirectory: string;aOptions: string): Integer;
+function codegenex(aProject: string; aProjectconfig: string; aVariables: string; aWorkingdirectory: string;aOptions: string; aPreCompatible: boolean): Integer;
 function GetcodegenPath: String;
-
 
 implementation
 
@@ -16,28 +16,38 @@ begin
   Result := File.IncludeTrailingPathDelimiter(Environment.GetEnvironmentVar('ZCODE'));
 end;
 
-function ccodegen(aProject: string; aProjectconfig: string; aVariables: string; aWorkingdirectory: string;aOptions: string): Integer;
+function codegen(aProject: string; aProjectconfig: string; aVariables: string; aWorkingdirectory: string;aOptions: string): Integer;
+begin
+  result := codegenex(aProject,aProjectconfig,aVariables,aWorkingdirectory,aOptions, false);
+end;
+
+function codegenex(aProject: string; aProjectconfig: string; aVariables: string; aWorkingdirectory: string;aOptions: string; aPreCompatible: boolean): Integer;
 var
   SB: TStringBuilder;
 begin
   Result := -1;
-
   
   if not Folder.Exists(GetcodegenPath) then 
     begin
-      RaiseException(erCustomError, 'CodeImatic.codegen Configured path [' + GetcodegenPath +'] cannot not be found.');
-
-      Exit;
+      if not aPreCompatible then 
+        RaiseException(erCustomError, 'CodeImatic.codegen Configured path [' + GetcodegenPath +'] cannot not be found.')
+      else  
+        RaiseException(erCustomError, 'zcodegen Configured path [' + GetcodegenPath +'] cannot not be found.');
     end;
 
   try
     SB:= TStringBuilder.Create;
 
-    SB.Append(GetcodegenPath + 'codeitmatic.codegen.exe ');
+    if not aPreCompatible then 
+       SB.Append(GetcodegenPath + 'codeitmatic.codegen.exe ')
+    else
+       SB.Append(GetcodegenPath + 'zcodegen.exe ');
+    
 
     SB.Append('-project ' + aProject + ' ');
     SB.Append('-projectconfig ' + aProjectconfig + ' ');
-    SB.Append('-consoleoutputonly ');
+
+    if not aPreCompatible then  SB.Append('-consoleoutputonly ');
 
     if aWorkingdirectory <> '' then 
       SB.Append('-workingdirectory '+  aWorkingdirectory+ ' ');   
@@ -47,7 +57,6 @@ begin
 
     if aOptions <> '' then
       SB.Append(aOptions + ' ');   
-
   
     Output.logformat('Running: %s', [SB.ToString]);
 
