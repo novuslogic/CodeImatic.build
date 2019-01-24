4a37af62cf30b16d20187c8ea0902ec8011b0f14
ajohnston <ajohnston@AIMS54-1>
2019-01-24 13:27:36 +1100

    Update MSWebDeploy.pas

--- source/Includes/MSWebDeploy.pas
+++ source/Includes/MSWebDeploy.pas
@@ -5,8 +5,10 @@ interface
 (*
 
 Web Deploy error codes
-
 https://docs.microsoft.com/en-us/iis/publish/troubleshooting-web-deploy/web-deploy-error-codes
+
+Web Deploy PowerShell Cmdlets
+https://docs.microsoft.com/en-us/iis/publish/using-web-deploy/packaging-and-restoring-a-web-site
 
 
 *)
