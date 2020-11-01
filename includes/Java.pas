195c492e136ca333e45e40c4771e45a15a51a285
Adam Craig Johnston <acj1971@users.noreply.github.com>
2020-11-01 21:40:38 +1100

    New Java Include file

---
+++ includes/Java.pas
@@ -0,0 +1,65 @@
+unit Java;
+
+interface
+
+uses Cmd;
+
+function GetJAVA_HOME: string;
+function GetJavaFolder: String;
+function GetJavaVersion: String;
+
+implementation
+
+function GetJavaVersion: String;
+begin
+  result := '';
+end;
+
+function GetJavaFolder: String;
+begin
+  result := File.IncludeTrailingPathDelimiter(GetJAVA_HOME);
+end;
+
+
+// Example: setx -m JAVA_HOME "C:\Program Files\AdoptOpenJDK\jdk-11.0.8.10-hotspot\bin"
+function GetJAVA_HOME: string;
+begin
+  result := Environment.GetEnvironmentVar('JAVA_HOME');
+
+  If Result = '' then
+    RaiseException(erCustomError, 'Invalid "JAVA_HOME" environment variable not set.');
+
+end;
+
+
+function JavaOutput
+
+function Java(const aFilename: string;const aOption: string): Integer;
+var
+  SB: TStringBuilder;
+begin
+   Result := -1;
+
+   if not File.Exists(GetJavaFolder + 'Java.exe') then 
+     RaiseException(erCustomError, 'Cannot find java.exe.');
+    
+   Try
+     SB:= TStringBuilder.Create;
+     SB.Append('"' + GetJavaFolder + 'Java.exe'  + '" ');
+
+     if aFilename <> '' then SB.Append('"' + aFilename  + '"');
+ 
+     if Trim(aOption) <> '' then SB.Append(aOption + ' ');
+  
+     Output.logformat('Running: %s', [SB.ToString]);
+
+     result := Exec(sb.ToString);
+  
+  finally
+    SB.Free;
+  End;
+ 
+end;
+
+
+end.
